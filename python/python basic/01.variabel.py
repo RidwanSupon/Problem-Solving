0f41c91df483e80f557fd94cbf2e7bcc392584f1
RidwanSupon <ridwansupon@gmail.com>
2024-03-16 23:03:46 +0600

    update

--- python/python basic/01.variabel.py
+++ python/python basic/01.variabel.py
@@ -1,3 +1,16 @@
 name = "Ridwan"
+age = 21
 print("I am "+name)
 print(name+" lives in Dhaka")
+print("he is currently",age,"years old")
+
+
+print("hello" + "world")
+
+
+#single comment
+
+
+'''
+multiple comments
+'''
