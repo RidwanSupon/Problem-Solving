28dfd9564e5738f296ea560d5253177e2a3f2d65
RidwanSupon <ridwansupon@gmail.com>
2024-03-17 00:23:02 +0600

    update

--- python/python basic/04.typecasting.py
+++ python/python basic/04.typecasting.py
@@ -1,5 +1,11 @@
 num1 = int(input("enter first num: "))
 num2 = int(input("enter second num: "))
-sum = num1+num2
 
-print("Sum is: ",sum)
+result = num1+num2
+print("Sum is: ",result)
+result = num1-num2
+print("Sub is: ",result)
+result = num1*num2
+print("Mul is: ",result)
+result = num1/num2
+print("Div is: ",result)
