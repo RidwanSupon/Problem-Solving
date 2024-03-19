58f60a0cfdadf8b838980103a02cf155cf2f35a4
RidwanSupon <ridwansupon@gmail.com>
2024-03-19 14:16:57 +0600

    update

---
+++ python/python basic/07.stringFormatting.py
@@ -0,0 +1,8 @@
+
+num1= 20
+num2= 30
+print(f"{num1} + {num2} = {num1 + num2}")
+
+
+print("Ridwanur rahman",end=" ")
+print("0151515151")
---
+++ python/python basic/08.relationalOperator.py
@@ -0,0 +1,17 @@
+ans = 20 > 30
+print(ans)
+
+ans = 20 < 30
+print(ans)
+
+ans = 20!=30
+print(ans)
+
+ans = "hello" == "hello"
+print(ans)
+
+ans = "hello" == "llo"
+print(ans)
+
+ans = 20>=20
+print(ans)
