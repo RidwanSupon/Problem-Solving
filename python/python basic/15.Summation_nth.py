4584aa8ff3edf373764d4153f948f8969f7df6af
RidwanSupon <ridwansupon@gmail.com>
2024-03-20 16:05:33 +0600

    update

--- python/python basic/15.Summation_nth.py
+++ python/python basic/15.Summation_nth.py
@@ -4,3 +4,10 @@ while i<=100:
     sum = sum + i
     i = i + 1
 print("1 to 100 numbers sum: ",sum)
+
+k=2
+sum=0
+while k<=100:
+    sum = sum + k
+    k = k+2
+print("2 to 100 even numbers sum: ",sum)
