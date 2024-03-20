3a166f8ceeb3d6aad67b55dee51fc1d5ce4c10af
RidwanSupon <ridwansupon@gmail.com>
2024-03-20 16:23:05 +0600

    update

---
+++ python/python basic/16.break_continue.py
@@ -0,0 +1,18 @@
+i=1
+while i<=50:
+    print(i,end=" ")
+    i=i+1
+    if i==21:
+        break
+print()
+
+
+
+j=1
+while j<=30:
+    if j==27:
+        j=j+1       #27 hobe then abar loop e back korbe 
+        continue
+    print(j,end=" ")
+    j=j+1
+    
