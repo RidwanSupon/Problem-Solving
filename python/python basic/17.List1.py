cbe79fa1d3368ea3fa8fe8ad77e034864b6c1217
RidwanSupon <ridwansupon@gmail.com>
2024-03-20 16:38:16 +0600

    update

---
+++ python/python basic/17.List1.py
@@ -0,0 +1,13 @@
+subjects = ["C","C++","java","python","OS","Mat"]
+
+print(subjects + ["swift","ABc",21]) # adding more element array 
+print(subjects) 
+print(subjects[0]) #index 0 
+
+print(subjects[2:]) # index num 2 theke shob print korbe
+print(subjects[-1])     #last index print korbe
+
+# check korbo suject e element ache kina
+
+print("python" in subjects) # its return boolean
+print("swift" not in subjects) 
