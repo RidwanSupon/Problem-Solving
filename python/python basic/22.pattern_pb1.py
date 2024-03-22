0d892aca11840c9451f0ca8fba77949a043569e5
RidwanSupon <ridwansupon@gmail.com>
2024-03-22 14:49:18 +0600

    update

---
+++ python/python basic/22.pattern_pb1.py
@@ -0,0 +1,14 @@
+
+"""
+*
+* *
+* * *
+* * * *
+
+"""
+
+n = int(input("Enter length: "))
+
+
+for i in range(n+1):
+    print(i*" *")
---
+++ python/python basic/23.pattern_pb2.py
@@ -0,0 +1,13 @@
+"""
+*
+***
+*****
+*******
+
+"""
+
+n = int(input("enter length: "))
+
+
+for i in range(n+1):
+    print((2*i-1)*"*")
