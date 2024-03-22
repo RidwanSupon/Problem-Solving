35b129698393311a00668ea4c63334b07fda3bd6
RidwanSupon <ridwansupon@gmail.com>
2024-03-22 14:16:13 +0600

    update

---
+++ python/python basic/19.rangeFunc.py
@@ -0,0 +1,10 @@
+num = list(range(10))
+print(num)
+print(num[2])   #indx 2 value 2
+
+num = list(range(5,11))
+print(num)
+
+
+num = list(range(2,101,2))  #2 to 100 times its print and increment 2
+print(num)
