2db5af2fb941649152e139224d03b634dbe21ec5
RidwanSupon <ridwansupon@gmail.com>
2024-03-24 00:04:18 +0600

    update

---
+++ python/python basic/31.sets.py
@@ -0,0 +1,23 @@
+# unorder collection of items
+#duplicate value allowed na. thakleo ekbar show korbe shudu
+
+
+num1 = {1,2,3,4,5,6,7,}
+print(num1)
+
+num2 = set([4,5,6])
+num2.add(7)
+print(num2)
+num2.remove(4)
+print(4 not in num2)
+print(4 in num2)
+
+
+#union
+print(num1 | num2)
+
+#intersection
+print(num1 & num2)
+
+#difference
+print(num1 - num2)
