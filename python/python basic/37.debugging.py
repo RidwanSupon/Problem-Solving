536769d17f5096eac76a40d80fd4562cb73bbec8
RidwanSupon <ridwansupon@gmail.com>
2024-03-27 03:19:41 +0600

    update

---
+++ python/python basic/37.debugging.py
@@ -0,0 +1,8 @@
+def add(*numbers):
+    sum = 0
+    for num in numbers:
+        sum = sum + num
+    return sum  # ekhane return er possition er jnno loop ekbar ghurei value return kore dibe.
+
+print(add(10,20,554))
+print(add(2,3))
---
+++ python/python basic/38.lamda_func.py
@@ -0,0 +1,4 @@
+#single line of code niye kaj kore 
+
+res = (lambda a,b : a*a + 2*a*b + b*b)(2,3)
+print(res)
