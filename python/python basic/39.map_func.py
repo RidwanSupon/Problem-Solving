dbe48ddd1098f0ce0062232c6edcea77ad3a844f
RidwanSupon <ridwansupon@gmail.com>
2024-03-27 03:26:14 +0600

    update

---
+++ python/python basic/39.map_func.py
@@ -0,0 +1,9 @@
+#powerful func
+# func & list recieve korbe
+
+def square(x):
+    return x*x
+
+num = [1,2,3,4]
+res = list(map(square,num))
+print(res)
---
+++ python/python basic/40.filter_func.py
@@ -0,0 +1,7 @@
+#filter func
+
+num = [1,2,3,4,5]
+
+res = list(filter(lambda x: x%2==0,num))
+
+print(res)
