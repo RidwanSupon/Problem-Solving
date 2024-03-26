b7d14e9e782de4fef2ec18c2b21d1ef7abbdcb0d
RidwanSupon <ridwansupon@gmail.com>
2024-03-27 02:46:02 +0600

    update

--- python/python basic/Competetive Programming Course/2.overflow1.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-
-    double a=100000;
-    double b=100000;
-    double c= a*b;  
-    cout<<fixed<<setprecision(0)<<c<<endl;  // scientific notation will remove by using fixed & set precious 0 will remove the floating value
-}
